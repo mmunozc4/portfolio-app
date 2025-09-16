a3f91335beb212f479c6538689028fb1069f9c80
mmunozc <mateomunozc@outlook.com>
2025-09-16 15:02:37 -0500

    finals changes

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -16,13 +16,20 @@ export class ProjectsService {
               La plataforma permite a las empresas registrar y almacenar la información de sus clientes, lo que agiliza la programación de futuras citas y contribuye a mantener un mayor control y orden en la atención.
 
               Es importante señalar que esta es una versión demo, por lo que algunas funcionalidades pueden encontrarse incompletas o en desarrollo. 
-              Sin embargo, la aplicación es completamente adaptable a las necesidades de cada cliente, desde la implementación de nuevas características hasta la personalización del diseño y flujo de trabajo.`,
+              Sin embargo, la aplicación es completamente adaptable a las necesidades de cada cliente, desde la implementación de nuevas características hasta la personalización del diseño y flujo de trabajo.
+              
+              <strong>Credenciales para ingresar a la demo  
+              Usuario: MykeMunozPA / Contraseña: 1234</strong>
+              `,
       repoLink: 'https://github.com/mmunozc4/appointmentsManager-app',
       appLink: 'https://mmunozc4.github.io/appointmentsManager-app/#/login',
       pictures: [
-        "assets/carrousel/citas-1.png",
-        "assets/carrousel/citas-2.png",
-        "assets/carrousel/citas-3.png"
+        "assets/carrousel/Appointments/1.png",
+        "assets/carrousel/Appointments/2.png",
+        "assets/carrousel/Appointments/3.png",
+        "assets/carrousel/Appointments/4.png",
+        "assets/carrousel/Appointments/5.png",
+        "assets/carrousel/Appointments/6.png"
       ],
       tags: [Tag.ANGULAR, Tag.NODEJS, Tag.NODEJS, Tag.SQL]
     },
@@ -38,28 +45,39 @@ export class ProjectsService {
       repoLink: 'https://github.com/mmunozc4/Inventory-app.git',
       appLink: 'https://inventory-app-fronend.onrender.com/analysis',
       pictures: [
-        "assets/carrousel/inventario-1.png",
-        "assets/carrousel/inventario-2.png",
-        "assets/carrousel/inventario-3.png"
+        "assets/carrousel/Inventory/1.png",
+        "assets/carrousel/Inventory/2.png",
+        "assets/carrousel/Inventory/3.png",
+        "assets/carrousel/Inventory/4.png",
+        "assets/carrousel/Inventory/5.png",
+        "assets/carrousel/Inventory/6.png"
       ],
       tags: [Tag.ANGULAR, Tag.NODEJS, Tag.PYTHON, Tag.POSTGRESQL]
     },
     {
       id: 2,
       name: 'Aplicación de Ecommerce',
-        summary: 'Plataforma de comercio electrónico para la gestión de productos, ventas en línea y panel administrativo exclusivo para el administrador.',
+      summary: 'Plataforma de comercio electrónico para la gestión de productos, ventas en línea y panel administrativo exclusivo para el administrador.',
       description: `Esta aplicación de Ecommerce fue desarrollada con el objetivo de ofrecer una plataforma intuitiva y eficiente para la gestión de productos y ventas en línea. 
           El diseño está pensado para brindar una experiencia de usuario fluida y moderna, permitiendo la navegación por categorías, visualización de productos y realización de compras.
 
           Es importante destacar que esta es una versión demo de la aplicación. En esta versión, algunas funcionalidades y detalles pueden no estar completamente implementados. 
           Además, aunque la aplicación ofrece una visión general del sistema, todas las características pueden ser modificadas y adaptadas a las necesidades específicas del cliente, desde la adición de nuevas funciones hasta la personalización del diseño. 
-          El acceso al panel de administración está restringido solo al administrador de la plataforma, habilitando el botón de inicio de sesión exclusivamente para usuarios autorizados.`,
+          El acceso al panel de administración está restringido solo al administrador de la plataforma, habilitando el botón de inicio de sesión exclusivamente para usuarios autorizados.
+          
+          <strong>Credenciales para ingresar a la demo 
+          Usuario: admin / Contraseña: 317809</strong>
+          
+          `,
       repoLink: 'https://github.com/mmunozc4/ecommerce-app.git',
-      appLink: 'https://ecommerce-app-mu-henna.vercel.app/#/shop/category-products',
+      appLink: 'https://ecommerce-app-mu-henna.vercel.app/#/shop',
       pictures: [
-        "assets/carrousel/ecommerce-1.png",
-        "assets/carrousel/ecommerce-2.png",
-        "assets/carrousel/ecommerce-3.png"
+        "assets/carrousel/Eccomerce/1.png",
+        "assets/carrousel/Eccomerce/2.png",
+        "assets/carrousel/Eccomerce/3.png",
+        "assets/carrousel/Eccomerce/4.png",
+        "assets/carrousel/Eccomerce/5.png",
+        "assets/carrousel/Eccomerce/6.png"
       ],
       tags: [Tag.ANGULAR, Tag.NODEJS, Tag.NODEJS, Tag.SQL]
     }
